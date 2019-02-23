bac13de706059e8f101fc8e15879d9658383e18f
deeem <karpov.dimon@gmail.com>
2019-02-23 15:11:07 +0200

    update route list

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('bits', 'v1\BitController');
+Route::apiResource('bits', 'v1\BitController');
+//Route::post('books/{book}/rating', 'RatingController@store');
