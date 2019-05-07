a41ade389db7fe32238947b1542411e11b90e2a0
deeem <karpov.dimon@gmail.com>
2019-05-07 17:38:52 +0300

    refactor login

--- app/Http/Controllers/v1/Login.php
+++ app/Http/Controllers/v1/Login.php
@@ -4,27 +4,27 @@ namespace App\Http\Controllers\v1;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Auth;
 
 class Login extends Controller
 {
     /**
      * Handle the incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function __invoke(Request $request)
     {
-        if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
-            $user = Auth::user();
-            $success['token'] =  $user->createToken('Password Token')-> accessToken;
-            $success['name'] = $user->name;
-            $success['email'] = $user->email;
-            return response()->json($success, 200);
-        }
-        else{
-            return response()->json(['error'=>'Unauthorised'], 401);
+        if (auth()->attempt(['email' => request('email'), 'password' => request('password')])) {
+            $user = auth()->user();
+
+            return response()->json([
+                'token' => $user->createToken('Password Token')->accessToken,
+                'name' => $user->name,
+                'email' => $user->email,
+            ], 200);
+        } else {
+            return response()->json(['error' => 'Unauthorised'], 401);
         }
     }
 }
